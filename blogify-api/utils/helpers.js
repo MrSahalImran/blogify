b79f48a7674b9e8f9a2de0d4a7ec60a6c2f3e135
sahal <sahalimran.pro@gmail.com>
2025-07-03 22:27:37 +0530

    User model

---
+++ blogify-api/utils/helpers.js
@@ -0,0 +1,22 @@
+export const UserRoleEnum = {
+  ADMIN: "admin",
+  USER: "user",
+};
+
+export const AvailableUserRole = Object.values(UserRoleEnum);
+
+export const AccountLevelEnum = {
+  BRONZE: "bronze",
+  SILVER: "silver",
+  GOLD: "gold",
+};
+
+export const AvailableAccountLevel = Object.values(AccountLevelEnum);
+
+export const UserGenderEnum = {
+  MALE: "male",
+  FEMALE: "female",
+  OTHER: "other",
+};
+
+export const AvailableGender = Object.values(UserGenderEnum);
