0c80ab81cab6dd8eff1fc735a582175a246fe5ce
sahal <sahalimran.pro@gmail.com>
2025-07-03 22:46:06 +0530

    Comment Model

---
+++ blogify-api/models/comment.models.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
